d9eabff5071bb94ade69b40c56b6d45d6c17d6ec
pani592 <pani592@aucklanduni.ac.nz>
2021-05-18 23:14:58 +1200

    Implemented Motorbike class and DataProvider for cruisers, added images

--- app/src/main/java/com/example/motorbikeapp/Motorbike.java
+++ app/src/main/java/com/example/motorbikeapp/Motorbike.java
@@ -1,4 +1,34 @@
 package com.example.motorbikeapp;
 
-public class Motorbike {
+import java.io.Serializable;
+
+public class Motorbike implements Serializable {
+    private String model;
+    private double price;
+    private String company;
+    private String category;
+    private int ImageAddr;
+
+    public Motorbike(String model, double price, String company, String category, int ImageAddr){
+        this.model = model;
+        this.price = price;
+        this.company = company;
+        this.category = category;
+        this.ImageAddr = ImageAddr;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getCompany(){
+        return company;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getImage(){
+        return ImageAddr;
+    }
 }
--- app/src/main/java/com/example/motorbikeapp/MotorbikeProvider.java
+++ app/src/main/java/com/example/motorbikeapp/MotorbikeProvider.java
@@ -1,4 +1,37 @@
 package com.example.motorbikeapp;
 
+import java.util.ArrayList;
+
 public class MotorbikeProvider {
+    static String[] models = {"Meteor 350", "CMX500 Rebel", "Vulcan S", "CMX1100 Rebel", "Scout 1200", "Softail Slim", "X-Diavel", "R18", "Rocket 3", "Vintage Dark Horse"};
+
+    static double[] prices = {3749, 5799, 6699, 8999, 11899, 16495, 16995, 18995, 20000, 21399};
+
+    static String[] companies = {"Royal Enfield", "Honda", "Kawasaki", "Honda", "Indian", "Harley-Davidson", "Ducati", "BMW", "Triumph", "Indian"};
+
+    static String[] categories = {"Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser", "Cruiser"};
+
+    //image path of bike images in the drawable folder
+    static int[] imageaddrs = {R.drawable.mc001a,
+            R.drawable.mc002a,R.drawable.mc003a,R.drawable.mc004a,R.drawable.mc005a,R.drawable.mc006a,
+            R.drawable.mc007a,R.drawable.mc008a,R.drawable.mc009a, R.drawable.mc010a,};
+
+    public static ArrayList<Motorbike> generateData(String categoryName) {
+        ArrayList<Motorbike> bikes = new ArrayList<Motorbike>();
+
+        for (int i = 0; i < 10; i++) {
+            String model = models[i];
+            String company = companies[i];
+            double price = prices[i];
+            String category =  categories[i];
+            int imageaddr = imageaddrs[i];
+
+            if (categoryName.contains(categories[i])) {
+                Motorbike aBike = new Motorbike(model, price, company, category, imageaddr);
+                bikes.add(aBike);
+            }
+        }
+        return bikes;
+    }
 }
+
