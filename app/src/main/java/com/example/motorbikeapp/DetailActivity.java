f38fed0b1a5ba6b530b75c532314c9f193807361
pani592 <pani592@aucklanduni.ac.nz>
2021-05-23 00:55:04 +1200

    Slide Activity transitions made cleaner, automatic and infinite scrolling

--- app/src/main/java/com/example/motorbikeapp/DetailActivity.java
+++ app/src/main/java/com/example/motorbikeapp/DetailActivity.java
@@ -1,10 +1,17 @@
 package com.example.motorbikeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView tvCompany;
     private TextView tvPrice;
     private ViewPager2 viewPager2;
+    private Handler slideHandler = new Handler(Looper.myLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,5 +62,48 @@ public class DetailActivity extends AppCompatActivity {
         int positionID = bike.getBikePositionID();
         List<ImageSlider> sliderItems = MotorbikeProvider.generateImages(positionID);
         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1- Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                slideHandler.removeCallbacks(slideRunnable);
+                slideHandler.postDelayed(slideRunnable, 3000); // slide duration 3 seconds
+            }
+        });
+    }
+
+    private Runnable slideRunnable = new Runnable(){
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        slideHandler.removeCallbacks(slideRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        slideHandler.postDelayed(slideRunnable, 3000);
     }
 }
--- app/src/main/java/com/example/motorbikeapp/SliderAdapter.java
+++ app/src/main/java/com/example/motorbikeapp/SliderAdapter.java
@@ -30,6 +30,9 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
     @Override
     public void onBindViewHolder(@NonNull SliderAdapter.SliderViewHolder holder, int position) {
         holder.setImage(sliderItems.get(position));
+        if(position == sliderItems.size() - 2) {
+            viewPager2.post(runnable);
+        }
 
     }
 
@@ -50,4 +53,12 @@ public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderView
             imageView.setImageResource(sliderItem.getImageAddr());
         }
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
 }
